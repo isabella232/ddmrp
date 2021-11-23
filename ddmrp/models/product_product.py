81af2eae6999ef23381d7e7dbeb4e14e9f6b58e0
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2021-11-23 15:05:43 +0100

    Disable unarchiving buffer on product activation
    Having the stock buffer automatically unarchived when the state of a
    product changes is dangerous because it can lead to product being
    ordered again when it should not anymore.
    
    The issue encountered seemed to be related to how Odoo handles product
    variant. And some changes in the product attribute line of a product
    would call a write on the active field of related products variant.
    
    An improvement that could be added (maybe coming soon) is to have on
    the product form the information of existing archived stock buffers.

--- ddmrp/models/product_product.py
+++ ddmrp/models/product_product.py
@@ -13,14 +13,7 @@ class Product(models.Model):
 
     def write(self, values):
         res = super().write(values)
-        if "active" in values:
-            buffers = self.env["stock.buffer"].search(
-                [
-                    ("product_id", "in", self.ids),
-                    "|",
-                    ("active", "=", True),
-                    ("active", "=", False),
-                ]
-            )
-            buffers.write({"active": values.get("active")})
+        if "active" in values and not values.get("active"):
+            buffers = self.env["stock.buffer"].search([("product_id", "in", self.ids)])
+            buffers.write({"active": False})
         return res
